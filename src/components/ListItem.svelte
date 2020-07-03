8b5c52e742b7a04465e49f674dc3413f2b2e5498
Silas <krausesilas@gmail.com>
2020-07-03 14:11:53 +0200

    Add statically cdn

--- src/components/ListItem.svelte
+++ src/components/ListItem.svelte
@@ -5,10 +5,11 @@ export let item;
 function handleClick(){
     dispatch('detail', { id: item.id || item.trackId })
 }
+item.image = `https://cdn.statically.io/img/${item.artworkUrl100.replace('https://', '')}?w=128&h=128&quality=90&f=auto`
 </script>
 
 <div class="flex py-2" on:click={handleClick}>
-    <img class="h-16 rounded-xl" src={item.artworkUrl100} alt="{item.name}">
+    <img class="h-16 rounded-xl" src={item.image} alt="{item.name}">
     <div class="mx-3 mt-1 leading-tight">
         <p class="font-medium clamp">{item.name || item.trackName}</p>
         <p class="text-sm text-gray-600 clamp" style="--lines: 1">{item.artistName}</p>
